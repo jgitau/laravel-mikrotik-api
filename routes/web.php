5137eaedbf29ee67feac3609bdaea15c162809ad
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-05-29 14:37:57 +0700

    :pencil: (fix): tidying up the routing web.php

--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,98 @@
 <?php
 
-use App\Http\Controllers\Backend\ClientController;
-use App\Http\Controllers\Backend\DashboardController;
-use App\Http\Controllers\Backend\Report\UserController;
-use App\Http\Controllers\Backend\ServiceController;
-use App\Http\Controllers\Backend\Setup\Administrator\AdminController;
-use App\Http\Controllers\Backend\Setup\Config\ConfigController;
-use App\Http\Controllers\Backend\Setup\Administrator\GroupController;
-use App\Http\Controllers\Backend\Setup\SetUrlRedirectController;
-use App\Http\Controllers\Backend\Setup\VoucherPrintSetupController;
-use App\Http\Controllers\Home\LoginController;
+// Importing necessary classes with grouped namespace feature
 use Illuminate\Support\Facades\Route;
-// Get DataTatable
-use App\Http\Livewire\Backend\Setup\Administrator\Admin\DataTable as DataTableAdmin;
-use App\Http\Livewire\Backend\Setup\Administrator\Group\DataTable as DataTableGroup;
-use App\Http\Livewire\Backend\Setup\Config\DataTable as DataTableConfig;
-use App\Http\Livewire\Backend\Setup\Config\HotelRoom\DataTable as DataTableHotelRoom;
+use App\Http\Controllers\{
+    Backend\DashboardController,
+    Backend\ClientController,
+    Backend\ServiceController,
+    Backend\Report\UserController,
+    Backend\Setup\Administrator\AdminController,
+    Backend\Setup\Administrator\GroupController,
+    Backend\Setup\Config\ConfigController,
+    Backend\Setup\SetUrlRedirectController,
+    Backend\Setup\VoucherPrintSetupController,
+    Home\LoginController
+};
 
-// Home / Login Page
+// Importing Livewire Controllers
+use App\Http\Livewire\Backend\Setup\Administrator\{
+    Admin\DataTable as DataTableAdmin,
+    Group\DataTable as DataTableGroup
+};
+use App\Http\Livewire\Backend\Setup\Config\{
+    DataTable as DataTableConfig,
+    HotelRoom\DataTable as DataTableHotelRoom
+};
+
+// Home / Login Page route
 Route::get('/', [LoginController::class, 'index'])->name('index');
 
-// Dashboard
-Route::middleware(['check.session.cookie'])->name('backend.')->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    // Clients Routes
-    Route::get('clients/list-clients', [ClientController::class, 'index'])->name('clients.list-clients');
+// Grouping routes that require check.session.cookie middleware
+Route::middleware(['check.session.cookie'])->group(function () {
 
-    // Services Routes
-    Route::get('services/list-services', [ServiceController::class, 'index'])->name('services.list-services');
+    // Route for dashboard page
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('backend.dashboard');
 
-    // Reports Routes
-    Route::get('reports/list-online-users', [UserController::class, 'index'])->name('reports.list-online-users');
+    // Route for clients list page
+    Route::get('clients/list-clients', [ClientController::class, 'index'])->name('backend.clients.list-clients');
 
-    // Set URL Redirect Routes
-    Route::get('setup/set-url-redirect', [SetUrlRedirectController::class, 'index'])->name('setup/set-url-redirect');
-    Route::get('setup/voucher-print-setup', [VoucherPrintSetupController::class, 'index'])->name('setup/voucher-print-setup');
-    // Administrator Group
-    Route::prefix('setup/admin/')->name('setup.admin.')->group(function () {
-        // List Admin Routes
-        Route::get('list-admins', [AdminController::class, 'index'])->name('list-admins');
-        // List Group Routes
-        Route::get('list-groups', [GroupController::class, 'index'])->name('list-groups');
-        // Add New Group Routes
-        Route::get('add-new-group', [GroupController::class, 'create'])->name('add-new-group');
+    // Route for services list page
+    Route::get('services/list-services', [ServiceController::class, 'index'])->name('backend.services.list-services');
+
+    // Route for online users list page in reports section
+    Route::get('reports/list-online-users', [UserController::class, 'index'])->name('backend.reports.list-online-users');
+
+    // *** ROUTING FOR SETUP ***
+    // Route for URL redirection and voucher print setup page, administrator and config setup
+    Route::prefix('setup/')->name('backend.setup.')->group(function () {
+
+        // Route for URL redirection setup page
+        Route::get('set-url-redirect', [SetUrlRedirectController::class, 'index'])->name('set-url-redirect');
+
+        // Route for voucher print setup page
+        Route::get('voucher-print-setup', [VoucherPrintSetupController::class, 'index'])->name('voucher-print-setup');
+
+        // Grouping routes related to administrator setup
+        Route::prefix('admin/')->name('admin.')->group(function () {
+            // Route for admins list page
+            Route::get('list-admins', [AdminController::class, 'index'])->name('list-admins');
+            // Route for groups list page
+            Route::get('list-groups', [GroupController::class, 'index'])->name('list-groups');
+            // Route for adding a new group
+            Route::get('add-new-group', [GroupController::class, 'create'])->name('add-new-group');
+        });
+
+        // Grouping routes related to configuration setup
+        Route::prefix('config/')->name('config.')->group(function () {
+            // Route for configs list page
+            Route::get('list-configs', [ConfigController::class, 'index'])->name('list-config');
+            // Route for hotel rooms configuration page
+            Route::get('list-configs/hotel_rooms', [ConfigController::class, 'hotel_rooms'])->name('hotel_rooms');
+        });
     });
 
-    // Config Group
-    Route::prefix('setup/config/')->name('setup.config.')->group(function () {
-        // List Config Routes
-        Route::get('list-configs', [ConfigController::class, 'index'])->name('list-config');
-        Route::get('list-configs/hotel_rooms', [ConfigController::class, 'hotel_rooms'])->name('hotel_rooms');
-    });
+    // *** ROUTING FOR LIVEWIRE SETUP DATATABLE ***
+    // Grouping routes related to getting datatable for both administrator and configurations
+    Route::prefix('livewire/backend/setup')->group(function () {
 
+        // Grouping routes related to getting datatable for administrator
+        Route::prefix('administrator')->group(function () {
+            // Route for getting datatable data for admins
+            Route::get('admin/getDataTable', [DataTableAdmin::class, 'getDataTable'])->name('admin.getDataTable');
+            // Route for getting datatable data for groups
+            Route::get('group/getDataTable', [DataTableGroup::class, 'getDataTable'])->name('group.getDataTable');
+        });
+
+        // Grouping routes related to getting datatable for configurations
+        Route::prefix('config')->group(function () {
+            // Route for getting datatable data for configurations
+            Route::get('getDataTable', [DataTableConfig::class, 'getDataTable'])->name('config.getDataTable');
+            // Route for getting datatable data for hotel rooms configuration
+            Route::get('hotelRoom/getDataTable', [DataTableHotelRoom::class, 'getDataTable'])->name('config.hotelRoom.getDataTable');
+        });
+    });
 });
-// Get DataTable List Admin
-Route::get('livewire/backend/setup/administrator/admin/getDataTable', [DataTableAdmin::class, 'getDataTable'])->name('admin.getDataTable');
-// Get DataTable List Group
-Route::get('livewire/backend/setup/administrator/group/getDataTable', [DataTableGroup::class, 'getDataTable'])->name('group.getDataTable');
-// Get DataTable Config Group
-Route::get('livewire/backend/setup/config/getDataTable', [DataTableConfig::class, 'getDataTable'])->name('config.getDataTable');
-// Get DataTable Hotel ROom Group
-Route::get('livewire/backend/setup/config/hotelRoom/getDataTable', [DataTableHotelRoom::class, 'getDataTable'])->name('config.hotelRoom.getDataTable');
 
-// Logout
+// Route for logout
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');

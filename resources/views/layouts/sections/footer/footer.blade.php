8a062509d8ab53913d3fd3190f6dded80efebc69
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-05-30 10:33:12 +0700

    :ambulance: (fix): fix typos in scss

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -3,9 +3,7 @@
     <div class="{{ (!empty($containerNav) ? $containerNav : 'container-fluid') }}">
         <div class="footer-container d-flex align-items-center justify-content-center py-2 flex-md-row flex-column">
             <div>
-                © <script>
-                    document.write(new Date().getFullYear())
-                </script>
+                © {{ date('Y') }}
                 , Created by ❤️ <a
                     href="{{ (!empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '') }}"
                     target="_blank" class="fw-semibold">{{ (!empty(config('variables.creatorName')) ?

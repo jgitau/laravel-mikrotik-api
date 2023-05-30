0fad6d0bf33f5a6b14dca2a21669e0c0e76d6c57
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-05-30 14:31:05 +0700

    :sparkles: (feat): create form create group

--- app/Http/Livewire/Backend/Setup/Administrator/Group/CreateGroup.php
+++ app/Http/Livewire/Backend/Setup/Administrator/Group/CreateGroup.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Backend\Setup\Administrator\Group;
 
 use App\Models\Group;
+use App\Services\Group\GroupService;
 use Livewire\Component;
 
 class CreateGroup extends Component
@@ -10,6 +11,12 @@ class CreateGroup extends Component
 
     // Define public variable
     public $name,$groupName;
+
+    // DataPermissions
+    public $dataPermissions;
+
+    // Permissions
+    public $permission = [];
 
     // Listeners
     protected $listeners = [
@@ -19,6 +26,8 @@ class CreateGroup extends Component
     // Validation Rules
     protected $rules = [
         'groupName' => 'required|min:4|max:60|unique:groups,name',
+        // Add validation rules for the permission
+        'permission.*' => 'in:0,1',
     ];
 
     // Validation Messages
@@ -27,6 +36,7 @@ class CreateGroup extends Component
         'groupName.min'         => 'Group Name must be at least 4 characters!',
         'groupName.max'         => 'Group Name can be a maximum of 60 characters!',
         'groupName.unique'      => 'Group Name already exist!',
+        'permission.*.in'      => 'The selected permission is invalid.',
     ];
     /**
      * updated
@@ -40,6 +50,23 @@ class CreateGroup extends Component
         // (only `text` for now), validate it
         $this->validateOnly($property);
     }
+
+    /**
+     * Mount the component.
+     *
+     * This function retrieves the current URL redirect setting using GroupService and assigns it to a public variable.
+     * @param GroupService $groupService The service to handle configuration related actions.
+     */
+    public function mount(GroupService $groupService)
+    {
+        $this->dataPermissions = $groupService->getDataPermissions();
+
+        // Initialize all permissions to 0
+        foreach ($this->dataPermissions as $permission) {
+            $this->permission[$permission->id] = '0';
+        }
+    }
+
 
 
     /**

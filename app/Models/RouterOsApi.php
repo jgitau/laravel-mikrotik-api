60cddcb8b162920faf7671d0765be1b56aa4111d
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-06-08 09:39:23 +0700

    :pencil2: (typos): change attempt timeout mikrotik

--- app/Models/RouterOsApi.php
+++ app/Models/RouterOsApi.php
@@ -13,7 +13,7 @@ class RouterOsApi extends Model
     var $connected = false; //  Connection state
     var $port      = 8728;  //  Port to connect to (default 8729 for ssl)
     var $ssl       = false; //  Connect using SSL (must enable api-ssl in IP/Services)
-    var $timeout   = 6000;     //  Connection attempt timeout and data read timeout
+    var $timeout   = 3;     //  Connection attempt timeout and data read timeout
     var $attempts  = 5;     //  Connection attempt count
     var $delay     = 3;     //  Delay between connection attempts in seconds
 

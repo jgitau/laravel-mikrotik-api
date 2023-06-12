4c5b640c8f758d90cff8ded6909c38c89ced3a69
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-06-12 14:26:51 +0700

    :pencil: (write): comments the code

--- app/Jobs/UpdateMikrotikStats.php
+++ app/Jobs/UpdateMikrotikStats.php
@@ -34,29 +34,36 @@ class UpdateMikrotikStats implements ShouldQueue
      */
     public function handle(MikrotikApiService $mikrotikApiService)
     {
+        // Retrieve the Mikrotik configuration settings.
         $config = MikrotikConfigHelper::getMikrotikConfig();
 
+        // Check if the configuration exists and no values are empty.
         if ($config && !in_array("", $config, true)) {
+
+            // Try to retrieve Mikrotik resource data using Mikrotik API service. On exception, set data to null.
             try {
+                // Retrieve data from Mikrotik router using Mikrotik API Curl service.
                 $data = $mikrotikApiService->getMikrotikResourceData($config['ip'], $config['username'], $config['password']);
             } catch (\Exception $e) {
                 $data = null;
             }
 
+            // Assign the retrieved data to respective variables. Use default values if the data is not available.
             $cpuLoad = $data['cpuLoad'] ?? 0;
             $uptime = $data['uptime'] ?? '0d 0:0:0';
             $freeMemory = $data['freeMemoryPercentage'] ?? '0%';
             $activeHotspots = $data['activeHotspot'] ?? 0;
 
-            // Save the data in cache
+            // Store data in cache for multiple data retrievals
             Cache::put('mikrotik.cpuLoad', $cpuLoad, 60); // Keep the data for 60 minutes
             Cache::put('mikrotik.uptime', $uptime, 60); // Keep the data for 60 minutes
 
-            // Session
+            // Store data in session for a single retrieval
             session([
                 'mikrotik.freeMemory' => $freeMemory,
                 'mikrotik.activeHotspots' => $activeHotspots,
             ]);
+
         }
     }
 }

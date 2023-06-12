9469a975b994daa76fd044737cc93f68949cb3f0
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-06-12 07:29:50 +0700

    :bug: (fix): fixing height chart in mobile

--- resources/assets/js/backend/dashboard/traffic-chart.js
+++ resources/assets/js/backend/dashboard/traffic-chart.js
@@ -47,6 +47,8 @@ document.addEventListener('DOMContentLoaded', function () {
       ]
     },
     options: {
+      responsive: true,
+      maintainAspectRatio: false,
       scales: {
         x: {
           type: 'realtime',
